1f0f7b85622aaa41800fb0d0eb0a516e3743c6a6
jinglongwang <lorenw12@163.com>
2021-06-24 16:31:37 +0800

    重构了目录

--- Py_study/read_rawdata.py
+++ Py_study/read_rawdata.py
@@ -1,7 +1,7 @@
 if __name__ == '__main__':
-    fo = open("../data/TLK_raw_data.txt",
+    fo = open("../data/no_git_folder/1v2-0622.txt",
               mode='r', newline='\r\n')
-    fo2 = open("../data/TLK_raw_data2.txt",
+    fo2 = open("../data/no_git_folder/TLK_raw_data4.txt",
                mode='w')
     fuhao = 0
     str = ""
@@ -18,8 +18,12 @@ if __name__ == '__main__':
             strary = str.split("#")
             for i in range(len(strary)-1):
                 #print("#"+strary[i])
-                fo2.write(strary[i].replace("..","")+"\r")
-
+                a = strary[i].replace("..","")
+                if a.startswith("#") :
+                    a = a
+                else:
+                    a = "#"+a
+                fo2.write(a + "\r")
             str = "#"+strary[len(strary)-1]
             #print("---re str:",str)
         elif str.count("#")>0 and str.count("$") >0:
@@ -31,20 +35,38 @@ if __name__ == '__main__':
                 strary1 = str.split("$")
                 for i in range(len(strary1)-1):
                     #print("$" +strary1[i])
-                    fo2.write(strary1[i].replace("..","") + "\r")
+                    a = strary1[i].replace("..","")
+                    if a.startswith("$"):
+                        a = a
+                    else:
+                        a = "$" + a
+                    fo2.write(a + "\r")
+
                 str = "$"+strary1[len(strary1)-1]
 
             elif str.find("#")>str.find("$"):
                 strary1 = str.split("#")
                 for i in range(len(strary1) - 1):
                     #print("#"+strary1[i])
-                    fo2.write(strary1[i].replace("..","") + "\r")
+                    a = strary1[i].replace("..","")
+
+                    if a.startswith("#"):
+                        a = a
+                    else:
+                        a = "#" + a
+                    fo2.write(a + "\r")
                 str = "#" + strary1[len(strary1) - 1]
         elif str.count("$") >=2:
             strary = str.split("$")
             for i in range(len(strary) - 1):
                 #print("$" +strary[i])
-                fo2.write(strary[i].replace("..","") + "\r")
+                #fo2.write( + "\r")
+                a = strary[i].replace("..","")
+                if a.startswith("$"):
+                    a = a
+                else:
+                    a = "$" + a
+                fo2.write(a + "\r")
                 #fo2.write(strary[i] + "\r\n")
             str = "$" + strary[len(strary) - 1]
             #print("---re str:", str)
