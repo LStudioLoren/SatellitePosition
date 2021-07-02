95f56384d54da937998fb6f69bd54fd36361e06a
jinglongwang <lorenw12@163.com>
2021-07-02 16:22:46 +0800

    2021/7/2: -重构： SINGLEPOINTPOSITION.py:将一些公共函数提取出来放到RTKCOMMON.py。 tool.py:将部分功能提取出来到RTKCOMMON.py -修复： SATPOS.py:修复了卫星位置计算 NAVDATA.py:修复了卫星星历中，时间格式转换的问题（rinex中卫星星历时间就是GPS时间，无需+闰秒进行转换）。 -新增： RTKCOMMON.py:将公共卫星计算函数整合在一起。

--- position/rtkpoint/RTKPOSITION.py
+++ position/rtkpoint/RTKPOSITION.py
@@ -1,5 +1,6 @@
-from position.common import solustion
-
+from position.singlepoint import SINGLEPOINTPOSITION
+from position.common import *
+import numpy as np
 class RTKPARAM():
     #基站坐标，ECEF-XYZ
     refPoint = []
@@ -15,6 +16,177 @@ class RTKPARAM():
 
 
 class RTKPoistion():
-    def exeRTKPositon(self):
-        print("start")
+    def exeRTKPositon(self,rtkParam,roverObs,baseObs,navDataList):
+        #先计算单点位置，获取首次有效定位、速度
+        rtkParam.sol = SINGLEPOINTPOSITION.SinglePointPosition().exesinglepoint(roverObs, navDataList, rtkParam.sol)
+
+        #baseNavList = []
+        #roverNavList = []
+        if len(baseObs.obsary) < 4:
+            rtkParam.sol.update(rtkParam.sol.X, len(baseObs.obsary), 0, 0)
+            return rtkParam.sol
+        if len(roverObs.obsary) < 4:
+            rtkParam.update(rtkParam.sol.X, len(roverObs.obsary), 0, 0)
+            return rtkParam.sol
+
+        #根据基站位置计算基于基站的卫星位置信息等
+        baseNavList = self.getNavList(baseObs,navDataList)
+        nbaseObs = len(baseObs.obsary)
+        nroverObs = len(roverObs.obsary)
+
+        #roverNavList = self.getNavList(roverObs,navDataList)
+
+        #进行基站的非差残差计算
+        if self.zdres(0,baseObs,len(baseObs.obsary),baseNavList,rtkParam.refPoint,1) != 1:
+            return rtkParam.sol
+        #获取移动站和基站的公共卫星号、对应各自obs数组的位置。
+        self.selectCommonSat(roverObs,baseObs,nroverObs,nbaseObs)
+
+
+
+
+    def getNavList(self,obs,navDataList):
+        navList = []
+        for k in range(len(obs.obsary)):
+            for i in range(len(navDataList)):
+                # 计算卫星位置；
+                if obs.obsary[k].prn == navDataList[i].prn:
+                    # 卫星观测量对应的L1伪距
+                    # 整理星历数据打包程nav对象，计算卫星位置pos，计算卫星种差dts，计算卫星位置及时钟方差vare
+                    print("卫星:",navDataList[i].prn,"位置：")
+                    navList.append(SATPOS.SatPos().getSatpos(navDataList[i], obs.t_obs,
+                                                                 obs.obsary[k].obsfrefList[0].P))
+                else:
+                    continue
+        return navList
+    def zdres(self,type,obs,nobs,navList,rr,index):
+
+        #for i in range(len(obs))
+        nfre = 2
+        y=[]
+        dant=[]
+        for i in range(nobs):
+            y_obs = []
+            dant.append(0)
+            for j in range(nfre*2):
+                y_obs.append(0)
+            y.append(y_obs)
+        print(y[0],y[0][1])
+        if tool.dot(rr) <=0 :
+            return 0
+        rr_ = rr
+        pos = [0.0,0.0,0.0]
+        pos = RTKCOMMON.eceftopos(rr_,pos)
+        #L1+L2双频计算
+        lam = [tool.CLIGHT / tool.FREQ1, tool.CLIGHT / tool.FREQ2]
+        for i in range(nobs):
+            r = RTKCOMMON.r_corr(navList[i],rr_)
+            #print(r)
+            if r <= 0 :
+                continue
+
+            e = RTKCOMMON.e_corr(navList[i],rr_)
+            azel = RTKCOMMON.satAzel(pos,e)
+            if azel[1] < 10*np.pi/180 :
+                continue
+
+            r += (-tool.CLIGHT)* navList[i].dts
+            print(r)
+            zhd = self.tropmodel(obs.t_obs,pos,[0,90*np.pi/180],0)
+            tropnmf = self.tropmapf_nmf(obs.gpsweek,obs.t_obs,pos,azel,0)
+            r += tropnmf*zhd
+            print(r,tropnmf,zhd)
+            for j in range(len(obs.obsary[i].obsfrefList)):
+                #for k in range():
+                if (obs.obsary[i].obsfrefList[j].L > 0.0) :
+                    y[i][j] = obs.obsary[i].obsfrefList[j].L*lam[j] - r - dant[i]
+                if (obs.obsary[i].obsfrefList[j].P > 0.0):
+                    y[i][j+nfre] = obs.obsary[i].obsfrefList[j].P -r - dant[i]
+
+        return 1
+    def tropmodel(self,time,pos,azel,humi):
+        temp0 = 15
+        if pos[2] <-100 or pos[2]>1E4 or azel[1] <= 0 :
+            return 0
+        hgt = (0 if pos[2]<0 else pos[2])
+
+        pres = 1013.25*np.power(1-2.2557E-5*hgt,5.2568)
+        temp = temp0 - 6.5E-3*hgt+273.16
+        e = 6.108*humi*np.exp((17.15*temp-4684)/(temp-38.45))
+
+        z = np.pi /2 -azel[1]
+        trph = 0.0022768 * pres / (1-0.00266*np.cos(2*pos[0])-0.00028*hgt/1E3)/np.cos(z)
+        trpw = 0.002277 *(1255/temp+0.05)*e/np.cos(z)
+        return trph+trpw
+
+    def tropmapf_nmf(self,gps_week,gps_sec,pos,azel,map):
+        coef = [
+            [1.2769934E-3, 1.2683230E-3, 1.2465397E-3, 1.2196049E-3, 1.2045996E-3],
+            [2.9153695E-3, 2.9152299E-3, 2.9288445E-3, 2.9022565E-3, 2.9024912E-3],
+            [62.610505E-3, 62.837393E-3, 63.721774E-3, 63.824265E-3, 64.258455E-3],
+
+            [0.0000000E-0, 1.2709626E-5, 2.6523662E-5, 3.4000452E-5, 4.1202191E-5],
+            [0.0000000E-0, 2.1414979E-5, 3.0160779E-5, 7.2562722E-5, 11.723375E-5],
+            [0.0000000E-0, 9.0128400E-5, 4.3497037E-5, 84.795348E-5, 170.37206E-5],
+
+            [5.8021897E-4, 5.6794847E-4, 5.8118019E-4, 5.9727542E-4, 6.1641693E-4],
+            [1.4275268E-3, 1.5138625E-3, 1.4572752E-3, 1.5007428E-3, 1.7599082E-3],
+            [4.3472961E-2, 4.6729510E-2, 4.3908931E-2, 4.4626982E-2, 5.4736038E-2]
+        ]
+        aht = [2.53E-5, 5.49E-3, 1.14E-3]
+
+        ah = [0,0,0]
+        aw = [0,0,0]
+        el = azel[1]
+        lat = pos[0]*180/np.pi
+        hgt = pos[2]
+
+        if el <= 0 :
+            return 0
+
+        ep = tool.GPST2EPOCH(gps_week,gps_sec)
+        y = (tool.dayofyear(ep)-28 )/365.25+(0.5 if lat< 0 else 0)
+        cosy = np.cos(2*np.pi*y)
+        lat = np.fabs(lat)
+
+        for i in range(3):
+            ah[i] = self.interpc(coef[i],lat) - self.interpc(coef[i+3],lat)*cosy
+            aw[i] = self.interpc(coef[i+6],lat)
+
+        dm = (1/np.sin(el)-self.mapf(el,aht[0],aht[1],aht[2]))*hgt/1E3
+
+        if map >0 :
+            map = self.mapf(el,aw[0],aw[1],aw[2])
+
+        return self.mapf(el,ah[0],ah[1],ah[2])+dm
+
+    def interpc(self,coef,lat):
+        i = int(lat/15)
+        if i < 1:
+            return coef[0]
+        elif i > 4:
+            return coef[4]
+        return coef[i-1]*(1 - lat/15 +i)+coef[i]*(lat/15 -i)
+    def mapf(self,el,a,b,c):
+        sinel = np.sin(el)
+        return (1+a/(1+b/(1+c)))/(sinel+(a/(sinel+b/(sinel+c))))
+
+    def selectCommonSat(self,roverObs,baseObs,nroverObs,nbaseObs):
+        satList=[]
+        irover = []
+        ibase = []
+        for i in range(nroverObs):
+            for j in range(nbaseObs):
+                if roverObs.obsary[i].prn == baseObs.obsary[j].prn:
+                    satList.append(roverObs.obsary[i].prn)
+                    irover.append(i)
+                    ibase.append(j)
+
+        print(satList,irover,ibase)
+
+
+
+
+
+
 
--- position/singlepoint/SINGLEPOINTPOSITION.py
+++ position/singlepoint/SINGLEPOINTPOSITION.py
@@ -1,6 +1,4 @@
-import position.common.tool as tool
-import position.common.SATPOS as SATPOS
-import position.common.solustion as solustion
+from position.common import *
 import numpy as np
 
 #单点定位计算
@@ -53,133 +51,13 @@ class SPPParm():
 
 #单点定位算法类
 class SinglePointPosition():
-    # 计算卫星的对应测量点的方位角；这是固定算法，需要将
-    def satAzel(self,pos, e):
-        # print("satAzel e = ",e)
-        az = 0
-        el = np.pi / 2
-        # enu = []
-        if pos[2] > -tool.RE_WGS84:
-            enu = self.xyz2enu(pos, e)
-            # print("enu = ",enu)
-            if enu[0] * enu[0] + enu[1] * enu[1] < 1E-12:
-                az = 0
-            else:
-                az = np.arctan2(enu[0], enu[1])
-            if az < 0.0:
-                az += 2 * np.pi
-            el = np.arcsin(enu[2])
-            # print("az = ",az,"el",el,enu[0],enu[1],enu[2])
-        return [az, el]
 
-    #将卫星的x、y、z坐标系转换为enu坐标系，公式是固定的；
-    #pos是接收机的BLH，e是接收机相对于卫星的几何向量
-    def xyz2enu(self,pos, e):
-        enu = []
-        E9 = [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
-        # xyz2enu
-        SINP = np.sin(pos[0])
-        COSP = np.cos(pos[0])
-        SINL = np.sin(pos[1])
-        COSL = np.cos(pos[1])
-        E9[0] = -SINL
-        E9[1] = -SINP * COSL
-        E9[2] = COSP * COSL
-        E9[3] = COSL
-        E9[4] = -SINP * SINL
-        E9[5] = COSP * SINL
-        E9[6] = 0.0
-        E9[7] = COSP
-        E9[8] = SINP
 
-        n = 3
-        k = 1
-        m = 3
 
-        for i in range(n):
-            d = 0
-            for j in range(k):
-                for x in range(m):
-                    # print("E9[",x+i*n,"] = ",E9[x+i*n],"e[",x,"] = ",e[x])
-                    d += E9[i + x * n] * e[x]  # E1*e[x]/(dot(e_x,e,y,e_z)+E2*e[y]/dot+E3*e[
-                # print(d)
-                enu.append(d)
-        # print(enu)
-        enu[0] /= np.sqrt(tool.dot(e))
-        enu[1] /= np.sqrt(tool.dot(e))
-        enu[2] /= np.sqrt(tool.dot(e))
-        # print(enu)
-        # print("asdfa enu =  [0.9832349746631751, -0.0017021136149081018, 0.18233509647993557]")
-        return enu
 
-    #将接收机位置从ecef转成pos（BLH格式）
-    def eceftopos(self,rr, pos):
-        # print("eceftopos: rr = ",rr)
-        e2 = tool.FE_WGS84 * (2 - tool.FE_WGS84)
-        v = tool.RE_WGS84
-        R2 = tool.dot2(rr)
-        zk = 0
-        z = rr[2]
-        # print(R2)
-        while (1):
 
-            if np.fabs(z - zk) >= 1E-4:
-                zk = z
-                sinp = z / np.sqrt(R2 + z * z)
-                v = tool.RE_WGS84 / np.sqrt(1 - e2 * sinp * sinp)
-                z = rr[2] + v * e2 * sinp
-                # print("zk = ",zk," z = ",z," np.fabs(z-zk) = ",np.fabs(z-zk))
-            else:
-                break;
 
-        # pos1 = pos2 = pos3 = 0
 
-        if R2 > 1E-12:
-            # print(np.sqrt(R2),z)
-            pos[0] = np.arctan(z / np.sqrt(R2))
-        else:
-            if R2 > 0:
-                pos[0] = np.pi / 2
-            else:
-                pos[0] = -np.pi / 2
-
-        if R2 > 1E-12:
-            pos[1] = np.arctan2(rr[1], rr[0])
-        else:
-            pos[1] = 0
-
-        pos[2] = np.sqrt(R2 + z * z) - v
-
-        #print("POS = ",pos)
-        return pos
-
-    # 计算伪距残差
-    def r_corr(self,nav, rr):
-
-        r = np.sqrt(tool.dot([nav.x - rr[0], nav.y - rr[1], nav.z - rr[2]]))
-        # 再加上
-        r += tool.OMGE * (nav.x * rr[1] - nav.y * rr[0]) / tool.CLIGHT
-        return r
-
-    #nav是星历数据，rr是当前接收机位置
-    #Xs就是卫星的x，Xu就是接收机的X
-    #原理中的（-1 / （P（t0）= 伪距））*（Xs - Xu）
-
-    def e_corr(self,nav, rr):
-        e = [0, 0, 0, 1]
-        #将卫星的x、y、z分别减去接收机的位置，得出接收机相对于卫星的x、y、z方向距离，即e数组
-        e[0] = nav.x - rr[0]
-        e[1] = nav.y - rr[1]
-        e[2] = nav.z - rr[2]
-        # r是e数组的距离
-        r = np.sqrt(tool.dot(e))
-        # print("r = ", r, "  e_before= ", e)
-        for i in range(3):
-            # 计算接收机到卫星的向量vector，就是x
-            # 这里是重点：将e数组中x、y、z除以距离r，e数组重新赋值为接收机相对于卫星的x、y、z的向量值。
-            e[i] = e[i] / r
-        # print("e_after= ", e)
-        return e
     #
     '''
     对接收机位置进行估算
@@ -219,8 +97,8 @@ class SinglePointPosition():
                 rr[i] = X[i]
             # dtr是
             dtr = X[3]
-            # rr是x，Y,z，pos是ecef的位置
-            pos = self.eceftopos(rr, pos)
+            # rr是x，Y,z，pos是blh的位置
+            pos = RTKCOMMON.eceftopos(rr, pos)
             spparm = self.rescode(nav_list, OBS_P, rr, pos, err, dtr, V, Var)
             # print("e = ",e_matrix)
             n = spparm.vn
@@ -258,12 +136,12 @@ class SinglePointPosition():
         e_matrix = []
         for i in range(len(nav_list)):
             # 伪距修正，根据卫星位置，减去估算的用户位置
-            r = self.r_corr(nav_list[i], rr)  # [nav_list[i].nav.x,nav_list[i].nav.y,nav_list[i].nav.z]
+            r = RTKCOMMON.r_corr(nav_list[i], rr)  # [nav_list[i].nav.x,nav_list[i].nav.y,nav_list[i].nav.z]
             # 计算接收机相对于卫星位置的向量数组
-            e = self.e_corr(nav_list[i], rr)
+            e = RTKCOMMON.e_corr(nav_list[i], rr)
             # 计算卫星的方位角和截止角
 
-            azel = self.satAzel(pos, e)
+            azel = RTKCOMMON.satAzel(pos, e)
 
             # 小于10度截止的的就不参与解算。
             if azel[1] < 0.173:
