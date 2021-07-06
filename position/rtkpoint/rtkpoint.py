e529446bd5a75669ec597c66dc06b33db2dc0c20
Loren Wong <lorenw12@163.com>
2021-07-06 15:04:06 +0800

    2021/7/6: -重构： SINGLEPOINTPOSITION.py:将一些公共函数提取出来放到RTKCOMMON.py。 tool.py:将部分功能提取出来到RTKCOMMON.py -修复： SATPOS.py:修复了卫星位置计算 NAVDATA.py:修复了卫星星历中，时间格式转换的问题（rinex中卫星星历时间就是GPS时间，无需+闰秒进行转换）。 -新增： RTKCOMMON.py:将公共卫星计算函数整合在一起。

--- position/rtkpoint/rtkpoint.py
+++ position/rtkpoint/rtkpoint.py
@@ -11,11 +11,12 @@ if __name__ == '__main__':
     nBase = len(baseObs)
     rtkParam = RTKPOSITION.RTKPARAM()
     rtkParam.initRefPoint(-2333329.0760,5383350.5903,2492875.2940)
-    for i in range(len(roverObs)):
-        print("开始处理时间：", time.time())
-        rtkParam.sol = SINGLEPOINTPOSITION.SinglePointPosition().exesinglepoint(roverObs[i], navDataList,rtkParam.sol)
-        print("GPS week = ",rtkParam.sol.gpsweek," GPS sec = ",rtkParam.sol.gpssec," POS = ",rtkParam.sol.X," NS = ",rtkParam.sol.ns,"AGE = ",rtkParam.sol.age," POS TYPE = ",rtkParam.sol.pos_type)
-        print("结束处理时间：", time.time())
+    RTKPOSITION.RTKPoistion().exeRTKPositon(rtkParam,roverObs[0],baseObs[0],navDataList)
+    # for i in range(len(roverObs)):
+    #     print("开始处理时间：", time.time())
+    #     rtkParam.sol = SINGLEPOINTPOSITION.SinglePointPosition().exesinglepoint(roverObs[i], navDataList,rtkParam.sol)
+    #     print("GPS week = ",rtkParam.sol.gpsweek," GPS sec = ",rtkParam.sol.gpssec," POS = ",rtkParam.sol.X," NS = ",rtkParam.sol.ns,"AGE = ",rtkParam.sol.age," POS TYPE = ",rtkParam.sol.pos_type)
+    #     print("结束处理时间：", time.time())
     # for i in range(len(baseObs)):
     #     print(baseObs[i].t_obs)
     #     for j in range(len(baseObs[i].obsary)):
